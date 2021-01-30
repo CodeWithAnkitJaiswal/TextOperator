69895fdc7df49c2ef47e9c6aced3cc58fe6a30b7
CodeWithAnkitJaiswal <78140180+CodeWithAnkitJaiswal@users.noreply.github.com>
2021-01-30 13:15:52 +0530

    updating site address

--- textutils/urls.py
+++ textutils/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('https://codewithankitjaiswal.github.io/textoperator/', admin.site.urls),
     path('', views.home, name='home'),
     path('contact', views.contact, name='contact'),
     path('blog', views.blog, name='blog')

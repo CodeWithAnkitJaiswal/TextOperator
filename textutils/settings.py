280880dc82a51440205181e7a47e3274e709743b
Ankit Jaiswal <78140180+codewithankitjaiswal9@users.noreply.github.com>
2021-01-31 09:53:58 +0530

    allowed all hosts and turn debug false

--- textutils/settings.py
+++ textutils/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'zw1$#$6n5p2*gd2pnh$$n1ay0$kos2)-(_b4bn#!j1f813p$gi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
